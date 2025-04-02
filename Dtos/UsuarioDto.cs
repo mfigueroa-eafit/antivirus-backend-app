163c645d6b38f06aa785fe5f4955081330e9fe7a
Marinellys Figueroa <marinellys.figueroa@syngenta.com>
2025-04-02 00:18:17 -0500

    Adding lastname and birthday to the user

--- Dtos/UsuarioDto.cs
+++ Dtos/UsuarioDto.cs
@@ -3,6 +3,10 @@ public class UsuarioDto {
 
     public string Nombre { get; set; } = null!;
 
+    public string Apellido { get; set; } = null!;
+
+    public string FechaNacimiento { get; set; } = null!;
+
     public string Correo { get; set; } = null!;
     public string Password { get; set; } = null!;
 
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -7,6 +7,11 @@ namespace Antivirus.Models;
 public class Usuario { 
     public int Id { get; set; } 
     public required string Nombre { get; set; } 
+
+    public required string Apellido { get; set; } 
+
+     public required string FechaNacimiento { get; set; } 
+
     public required string Correo { get; set; } 
     public required string Password { get; set; } 
     public required string Rol { get; set; } 
